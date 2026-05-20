92006d5f417b286904990d39874f8e5c7bb4b537
jainitrao07 <jainitrao07@gmail.com>
2026-05-20 18:34:36 +0530

    Create labmanualq2.cpp

---
+++ labmanualq2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius;
+    cout<<"Enter the radius: "<<endl;
+    cin>> radius;
+    cout<<"The area of the circular fountain is: "<<3.14*radius*radius<<endl;
+    return 0;
+}
