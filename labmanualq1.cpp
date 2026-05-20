9fd8bcfcd1d0f2727b30ae5bedf1c21f290f3257
jainitrao07 <jainitrao07@gmail.com>
2026-05-20 18:23:20 +0530

    Create labmanualq1.cpp

---
+++ labmanualq1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float marks1, marks2, marks3;
+    cout<<"Enter the marks of first student: "<<endl;
+    cin>> marks1;
+    cout<<"Enter the marks of second student: "<<endl;
+    cin>> marks2;
+    cout<<"Enter the marks of third student: "<<endl;
+    cin>> marks3;
+
+    cout<<"The average marks is: "<< (marks1+marks2+marks3)/3;
+    return 0;
+}
